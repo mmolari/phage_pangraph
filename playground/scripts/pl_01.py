b72178f70f2c651b88d58025eb179c8897dce71e
Marco Molari <mmolari91@gmail.com>
2022-12-15 17:19:48 +0100

    hierarchical clustering

--- playground/scripts/pl_01.py
+++ playground/scripts/pl_01.py
@@ -5,33 +5,11 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 
 from scipy.cluster.hierarchy import dendrogram
-from sklearn.cluster import AgglomerativeClustering
-
-
-def plot_dendrogram(model, **kwargs):
-    # Create linkage matrix and then plot the dendrogram
-
-    # create the counts of samples under each node
-    counts = np.zeros(model.children_.shape[0])
-    n_samples = len(model.labels_)
-    for i, merge in enumerate(model.children_):
-        current_count = 0
-        for child_idx in merge:
-            if child_idx < n_samples:
-                current_count += 1  # leaf node
-            else:
-                current_count += counts[child_idx - n_samples]
-        counts[i] = current_count
-
-    linkage_matrix = np.column_stack(
-        [model.children_, model.distances_, counts]
-    ).astype(float)
-
-    # Plot the corresponding dendrogram
-    dendrogram(linkage_matrix, **kwargs)
+from scipy.cluster import hierarchy
 
 # %%
 mash_dist = "../../results/covid/mash_dist.csv"
+# mash_dist = "../../results/mash_triangle/autographiviridae.csv"
 df = pd.read_csv(mash_dist)
 df = df.pivot(index="strain_1", columns="strain_2", values="mash_dist")
 accs = df.columns.to_numpy()
@@ -42,17 +20,17 @@ dist = df.to_numpy()
 plt.matshow(dist)
 # %%
 # setting distance_threshold=0 ensures we compute the full tree.
-model = AgglomerativeClustering(distance_threshold=0., n_clusters=None, linkage="complete")
+Z = hierarchy.linkage(dist, method="ward", optimal_ordering=True)
+dendrogram(Z, leaf_label_func=lambda k: accs[k])
 
-model = model.fit(1-dist)
 plt.title("Hierarchical Clustering Dendrogram")
 # plot the top three levels of the dendrogram
-plot_dendrogram(model, truncate_mode="level", p=3)
-plt.xlabel("Number of points in node (or index of point if no parenthesis).")
+# plt.xlabel("Number of points in node (or index of point if no parenthesis).")
 plt.show()
 
+
 # %%
-order = model.labels_
+order = hierarchy.leaves_list(Z)
 
 plt.matshow(dist[order][:,order])
 plt.colorbar()
