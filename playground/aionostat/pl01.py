10d4769c43e0801dfe72aca4021ddd3e55cf9e1d
Marco Molari <mmolari91@gmail.com>
2024-11-14 17:39:20 +0100

    aionostat plot

--- playground/aionostat/pl01.py
+++ playground/aionostat/pl01.py
@@ -14,6 +14,7 @@ graph_fname = "../../results/pangraph/aionostat/20-0-0-polished.json"
 pan = pp.Pangraph.load_json(graph_fname)
 bdf = pan.to_blockstats_df()
 ref_iso = {
+    "hybrid_ref": "hybrid_ref",
     "MaxBurger": "MZ501093",
     "WalterGehring": "MZ501111",
 }
@@ -46,7 +47,7 @@ def segment(ax, s, e, b):
     #     ax.fill_between([s, e], 0, freq, color="C0", alpha=1, zorder=-2)
 
 
-fig, axs = plt.subplots(2, 1, figsize=(12, 5))
+fig, axs = plt.subplots(3, 1, figsize=(15, 7.5))
 
 
 for axi, (name, iso) in enumerate(ref_iso.items()):
@@ -73,7 +74,42 @@ for axi, (name, iso) in enumerate(ref_iso.items()):
 
 sns.despine()
 plt.tight_layout()
-fig.savefig(fig_fld / "where_core.png")
+fig.savefig(fig_fld / "where_core.png", dpi=300)
+plt.show()
+
+# %%
+
+fig, axs = plt.subplots(3, 1, figsize=(15, 7.5))
+
+name = "hybrid_ref"
+iso = ref_iso[name]
+for axi, (x0, x1) in enumerate([(0, 1 / 3), (1 / 3, 2 / 3), (2 / 3, 1)]):
+    x0, x1 = x0 * Ls["hybrid_ref"], x1 * Ls["hybrid_ref"]
+
+    ax = axs[axi]
+    path = pan.paths[iso]
+    B = path.block_ids
+    P = path.block_positions
+
+    for i, b in enumerate(B):
+        s, e = P[i], P[i + 1]
+        if e > s:
+            segment(ax, s, e, b)
+        else:
+            for pair in ((s, Ls[name]), (0, e)):
+                segment(ax, *pair, b)
+
+    ax.set_title(name)
+    ax.set_xlabel("genome position (bp)")
+    ax.set_ylabel("block frequency")
+    ax.set_ylim(0, 1)
+    ax.set_xlim(0, Ls[name])
+    ax.grid(axis="x", alpha=0.2)
+    ax.set_xlim(x0, x1)
+
+sns.despine()
+plt.tight_layout()
+fig.savefig(fig_fld / "hybrid_ref_zoom.png", dpi=300)
 plt.show()
 
 # %%
